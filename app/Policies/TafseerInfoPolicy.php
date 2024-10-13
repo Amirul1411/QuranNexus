d4eac993cdaa14be7d0e6a01c0a5179a5a6acf14
Amirul Asyraf <amirulasyraf1411@gmail.com>
2024-10-13 15:58:26 +0800

    Added audio recitation, tafseer, and translation info policies

--- app/Policies/TafseerInfoPolicy.php
+++ app/Policies/TafseerInfoPolicy.php
@@ -2,15 +2,64 @@
 
 namespace App\Policies;
 
+use App\Models\TafseerInfo;
 use App\Models\User;
 
 class TafseerInfoPolicy
 {
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view any models.
      */
-    public function __construct()
+    public function viewAny(User $user): bool
     {
-        //
+        return $user->isAdmin() || $user->isEditor();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, TafseerInfo $tafseerInfo): bool
+    {
+        return $user->isAdmin() || $user->isEditor();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isAdmin() || $user->isEditor();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, TafseerInfo $tafseerInfo): bool
+    {
+        return $user->isAdmin() || $user->isEditor();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, TafseerInfo $tafseerInfo): bool
+    {
+        return $user->isAdmin() || $user->isEditor();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, TafseerInfo $tafseerInfo): bool
+    {
+        return $user->isAdmin() || $user->isEditor();
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, TafseerInfo $tafseerInfo): bool
+    {
+        return $user->isAdmin() || $user->isEditor();
     }
 }
