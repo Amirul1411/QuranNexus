45e2b425d49748060eae9a147bdfea2caafb83c4
Amirul Asyraf <amirulasyraf1411@gmail.com>
2024-10-19 23:44:07 +0800

    Removed alpinejs dependency from package.json

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,1 @@
-// import './bootstrap';
-
-// import Alpine from 'alpinejs';
-// import focus from '@alpinejs/focus';
-
-// // Register the focus plugin
-// Alpine.plugin(focus);
-
-// window.Alpine = Alpine
-
-// Alpine.start()
+import './bootstrap';
